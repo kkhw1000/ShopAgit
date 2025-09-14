30f59482900ae9c03c68bb779dfbad36403bd77d
kkhw1000 <kkhw1000@naver.com>
2025-09-14 17:53:43 +0900

    first commit

---
+++ ShopAgit/src/main/java/com/hanul/shop/ShopAgit/ShopAgitApplication.java
@@ -0,0 +1,13 @@
+package com.hanul.shop.ShopAgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAgitApplication.class, args);
+	}
+
+}
---
+++ ShopAgit/src/test/java/com/hanul/shop/ShopAgit/ShopAgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanul.shop.ShopAgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
